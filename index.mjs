d81b426262aa388bc63b713226741622336cff9c
Tristan-2i <78900902+Tristan-2i@users.noreply.github.com>
2024-03-05 01:57:55 -0800

    V2

---
+++ index.mjs
@@ -0,0 +1,6 @@
+//import { db } from "./src/services/database.service";
+import { ObjectId } from "mongodb";
+import { collections } from "./src/services/database.service";
+
+console.log("Hello World!");
+d
