55e949c2818784395189fca28eb48b1e389d7d6f
Alexis Deville <alexis.deville@ig2i.centralelille.fr>
2024-03-28 18:12:25 +0100

    Edit biens et reservations

--- src/noSqlModels/tiers.js
+++ src/noSqlModels/tiers.js
@@ -9,29 +9,10 @@ const tierSchema = new database.Schema({
     mdp: { type: String },
     typeTiers: { type: String },
     questions: {
-        type: [{
-            idQuestion: { type: database.Schema.Types.ObjectId, required: true },
-            idAuteur: { type: database.Schema.Types.ObjectId, required: true },
-            idBien: { type: database.Schema.Types.ObjectId, required: true },
-            idSource: { type: database.Schema.Types.ObjectId, required: true },
-            texte: { type: String }
-        }]
+        type: [Question.QuestionModel.questionSchema]
     },
     reservations: {
-        type: [{
-            idReservation: { type: database.Schema.Types.ObjectId, required: true },
-            idBien: { type: database.Schema.Types.ObjectId, required: true },
-            idTiers: { type: database.Schema.Types.ObjectId, required: true },
-            dateDebut: { type: Date },
-            dateFin: { type: Date },
-            tarifQuotion: { type: Number },
-            montantTotal: { type: Number },
-            dateReservation: { type: Date },
-            dateReglement: { type: Date },
-            etat: { type: String },
-            commentaire: { type: String },
-            reponseCommentaire: { type: String }
-        }]
+        type: [Reservation.ReservationModel.reservationSchema]
     },
 });
 
@@ -48,5 +29,6 @@ function createRandomTier() {
 
     return tier;
 }
+
 
 module.exports = {TierModel, createRandomTier};
